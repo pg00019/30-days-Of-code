b28d52eb08bfc0a2fbd77c5c4333f23c6aa7ebad
Priya Gupta <58894967+pg00019@users.noreply.github.com>
2021-04-24 23:11:32 +0530

    Create #day13AbstractClass.java

---
+++ #day13AbstractClass.java
@@ -0,0 +1,16 @@
+#30DaysOfCode on HACKERRANK
+
+
+#Task
+Given a Book class and a Solution class, write a MyBook class that does the following:
+
+Inherits from Book
+Has a parameterized constructor taking these  parameters:
+string 
+string 
+int 
+Implements the Book class' abstract display() method so it prints these  lines:
+, a space, and then the current instance's .
+, a space, and then the current instance's .
+, a space, and then the current instance's .
+Note: Because these classes are being written in the same file, you must not use an access modifier (e.g.: ) when declaring MyBook or your code will not execute.
